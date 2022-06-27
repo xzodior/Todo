dbe58769fd2ef99dc578197eed026895cf6594ef
sadiqur rahman <sadiqur161001@gmail.com>
2022-06-27 21:59:51 +0100

    Attached an event listener to an li element where if clicked, it will show elements that have a check mark and hide the ones that don't

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const list = document.querySelector('#todo');
 const form = document.querySelector('.input');
+const check = document.querySelectorAll('.check');
+const completed = document.querySelector('.completed');
+const all = document.querySelector('.all')
+console.log(check)
 
 const generateItem = input => {
 
     const html = 
-    `<li class='items'>
-        <span>${input}</span>
-        <i class="fa-brands fa-xbox delete"></i>
+    `<li class="items">
+    <div class="align">
+        <input type="checkbox" name="check" class="check">
+        <label for="check">${input}</label>
+    </div>
+    <i class="fa-brands fa-xbox delete"></i>
     </li>`
 
     list.innerHTML += html;
@@ -29,3 +36,14 @@ list.addEventListener('click', event => {
         event.target.parentElement.remove();
     }
 })
+
+completed.addEventListener('click', event => {
+    Array.from(list.children).forEach(item => {
+        if(!item.firstElementChild.firstChild.nextSibling.checked){     
+            item.style.display = 'none';
+        }
+    })
+})
+
+// When clicking on complete, i use this method to check if each item is not checked.
+// if it isn't, display none will be toggled to hide the element.
