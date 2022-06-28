63d5c9e7623e90714d08dcb37ac28f68a947c911
sadiqur rahman <sadiqur161001@gmail.com>
2022-06-28 18:14:13 +0100

    Applied event listeners to li compenents at the bottom of the llist. Implemented a function which tracks the amount of items in the list which decreases if item is deleted and increases if an item is added.

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const list = document.querySelector('#todo');
 const form = document.querySelector('.input');
 const check = document.querySelectorAll('.check');
 const completed = document.querySelector('.completed');
-const all = document.querySelector('.all')
-console.log(check)
+const all = document.querySelector('.all');
+const active = document.querySelector('.active')
+const clearCompleted = document.querySelector('.clear-completed');
+const itemCounter = document.querySelector('.number')
+console.log(itemCounter)
 
 const generateItem = input => {
 
@@ -17,6 +20,8 @@ const generateItem = input => {
     </li>`
 
     list.innerHTML += html;
+    count++;
+    updateDisplay();
 }
 
 form.addEventListener('submit', event => {
@@ -34,6 +39,8 @@ form.addEventListener('submit', event => {
 list.addEventListener('click', event => {
     if(event.target.classList.contains('delete')){
         event.target.parentElement.remove();
+        count--;
+        updateDisplay();
     }
 })
 
@@ -41,9 +48,61 @@ completed.addEventListener('click', event => {
     Array.from(list.children).forEach(item => {
         if(!item.firstElementChild.firstChild.nextSibling.checked){     
             item.style.display = 'none';
+        } else if (item.firstElementChild.firstChild.nextSibling.checked){
+            item.style.display = 'flex';
         }
     })
 })
 
+// ^
 // When clicking on complete, i use this method to check if each item is not checked.
 // if it isn't, display none will be toggled to hide the element.
+
+all.addEventListener('click', event => {
+    Array.from(list.children).forEach(item => {
+        return item.style.display = 'flex';
+    })
+})
+
+// ^
+// similar to the method above but would showcase all items.
+
+
+active.addEventListener('click', event => {
+    Array.from(list.children).forEach(item => {
+        if(!item.firstElementChild.firstChild.nextSibling.checked){     
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    })
+})
+
+// ^
+// Same method applied to the completed event just swapped return values.
+
+clearCompleted.addEventListener('click', event => {
+    Array.from(list.children).forEach(item => {
+        if(item.firstElementChild.firstChild.nextSibling.checked){     
+            item.remove();
+            count--;
+            updateDisplay();
+        } 
+    })
+})
+
+// ^
+// remove() to completely remove the item from the DOM
+
+let count = 0;
+
+function updateDisplay (){
+    itemCounter.innerHTML = count;
+}
+
+updateDisplay();
+
+// ^
+// Created a function which would track how many items are in the todo.
+// This function will be called inside event listeners that remove or add an item.
+// to which the counter will update.
